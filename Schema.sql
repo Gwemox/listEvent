907b9e65cdef7692920dacec67d64de0340732da
BILLEMAZ Maxime <maxime.billemaz@ynov.com>
2017-12-20 17:21:30 +0100

    change MCD files

--- Schema.sql
+++ Schema.sql
@@ -1,21 +1,21 @@
 create table groupe (
-	id INT auto_increment not null,
+	id INT not null auto_increment,
 	label varchar2(15),
-	description varchar2(50)
-	PRIMARY KEY (ID)
+	description varchar2(50),
+	PRIMARY KEY (id)
 );
 
 create table address (
 	id INT auto_increment not null,
-	address varchar2(50)
-	PRIMARY KEY (ID)
+	address varchar2(50),
+	PRIMARY KEY (id)
 );
 
 create table identification (
 	id INT auto_increment not null,
 	login varchar2(30) not null,
-	password varchar2(100) not null
-	PRIMARY KEY (ID)
+	password varchar2(100) not null,
+	PRIMARY KEY (id)
 );
 
 create table users (
@@ -23,9 +23,22 @@ create table users (
 	name varchar2(15),
 	surname varchar2(15),
 	birth_date date,
-	PRIMARY KEY (ID),
-	FOREIGN KEY (groupID) REFERENCES groupe(id)
-	FOREIGN KEY (addressID) REFERENCES address(id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (groupID) REFERENCES groupe(id),
 	FOREIGN KEY (identificationID) REFERENCES identification(id)
 	
+);
+
+create table user_address_facturation(
+	id INT auto_increment not null,
+	PRIMARY KEY (id),
+	FOREIGN KEY (userID) REFERENCES users(id),
+	FOREIGN KEY (addressID) REFERENCES address(id)
+);
+
+create table user_address_livraison(
+	id INT auto_increment not null,
+	PRIMARY KEY (id),
+	FOREIGN KEY (userID) REFERENCES users(id),
+	FOREIGN KEY (addressID) REFERENCES address(id)
 );
